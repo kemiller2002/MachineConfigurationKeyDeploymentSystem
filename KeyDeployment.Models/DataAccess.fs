23dc609e90d52658d8e424ed6f45a5e2228e98e2
Kevin Miller <kemiller2002@gmail.com>
2014-12-20 14:55:55 -0500

    projects.

---
+++ KeyDeployment.Models/DataAccess.fs
@@ -0,0 +1,4 @@
+﻿namespace KeyDeployment.Models
+
+type Class1() = 
+    member this.X = "F#"
--- Tables/KeyDeployment.Frameworks.sql
+++
@@ -1,9 +0,0 @@
-IF NOT EXISTS(SELECT * FROM sys.tables t join sys.schemas s ON t.name = 'Frameworks' and s.name = 'KeyDeployment')
-BEGIN
-	CREATE TABLE KeyDeployment.Frameworks
-	(
-		FrameworkId INT NOT NULL PRIMARY KEY
-		,Name VARCHAR(50) NOT NULL
-		,FilePath VARCHAR(250) NOT NULL
-	)
-END
